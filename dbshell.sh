70b62f9e4a2de57c0db2fa47fb834e4981d828f8
Mark Shead <mark.shead@xeric.net>
2022-02-23 12:55:06 -0600

    updated to more current versions of flyway and h2.

--- dbshell.sh
+++ dbshell.sh
@@ -1,1 +1,1 @@
-java -cp ~/.m2/repository/com/h2database/h2/1.4.197/h2*.jar org.h2.tools.Shell -url jdbc:h2:./target/flyway -user sa
+java -cp ~/.m2/repository/com/h2database/h2/1.4.199/h2*.jar org.h2.tools.Shell -url jdbc:h2:./target/flyway -user sa
